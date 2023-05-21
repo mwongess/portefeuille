c92730be0b93a53f8010c26964083c75516e0911
mwongess <amosmwongelah@gmail.com>
2023-05-21 15:19:42 +0300

    Update UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,25 @@
 import { Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
 import "./App.css";
 import { Header } from "./components/Header/Header";
 import { Landing } from "./components/Landing/Landing";
 function App() {
   return (
     <>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="main">
-              <Header />
-              <Landing />
-            </div>
-          }
-        />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="main">
+                <Header />
+                <Landing />
+              </div>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
