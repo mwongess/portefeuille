6ec2aab950b33967ecc96c2a70cdfc74134bfb2a
mwongess <amosmwongelah@gmail.com>
2023-05-21 15:12:31 +0300

    Update UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
+import { Routes,Route } from "react-router-dom";
 import "./App.css";
 import { Header } from "./components/Header/Header";
 import { Landing } from "./components/Landing/Landing";
 function App() {
   return (
     <>
-      <div className="main">
-        <Header />
-        <Landing />
-      </div>
+     <div className="main">
+          <Header />
+          <Landing />
+        </div>
+      <Routes>
+        <Route />
+       
+      </Routes>
     </>
   );
 }
