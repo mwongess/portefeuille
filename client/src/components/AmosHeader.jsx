fd2a205824009c74e00819b3cf1d44b8c30d974b
mwongess <amosmwongelah@gmail.com>
2023-05-21 13:12:45 +0300

    Configure routing for my pages

--- client/src/components/AmosHeader.jsx
+++ client/src/components/AmosHeader.jsx
@@ -1,1 +1,17 @@
-export const AmosHeader =()=> {}
+export const AmosHeader =()=> {
+    return(
+        <>
+        <div>
+            <div>
+                <h4>AeMG</h4>
+            </div>
+            <div>
+                <p><Link to="">Portfolio</Link></p>
+                <p><Link to="">About</Link></p>
+                <p><Link to="">Contact</Link></p>
+                <button>Hire Me</button>
+            </div>
+        </div>
+        </>
+    )
+}
