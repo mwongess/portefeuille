5b33c68b09c502ff6dd552000140ad25b37190b2
mwongess <amosmwongelah@gmail.com>
2023-05-21 17:58:58 +0300

    Update intro

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -6,7 +6,7 @@ export const Landing = () => {
       <div className="landing">
         <h1>Amos Mwongela</h1>
         <h3>Software Engineer | Data Scientist</h3>
-        <p>
+        {/* <p>
           I possess a comprehensive understanding of both frontend and backend
           development.
         </p>
@@ -19,7 +19,22 @@ export const Landing = () => {
           On the backend, I am well-versed with languages such as NodeJs and
           NestJs, enabling me to build robust and{" "}
         </p>
-        <p>scalable server-side applications.</p>
+        <p>scalable server-side applications.</p> */}
+
+        <p>
+        Are you looking for a skilled and versatile full stack developer to bring your digital projects to life?.Look no further than me.
+        </p>
+        <p> With a passion for coding and a talent for creating innovative solutions, Im your go-to professional when it comes to web and software development.</p>
+        {/* <p>
+          My proficiency in Angular,React, Vue & SvelteKit allows me to craft
+          visually stunning and user-friendly interfaces that captivate
+          audiences.
+        </p>
+        <p>
+          On the backend, I am well-versed with languages such as NodeJs and
+          NestJs, enabling me to build robust and scalable server-side
+          applications.
+        </p> */}
         <div className="buttons">
           <button className="linkedin">
             <a href="https://www.linkedin.com/in/mwongess" target="_blank">
