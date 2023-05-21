e1ea63dc0cd25db208ea0838576900fdca24ce7a
mwongess <amosmwongelah@gmail.com>
2023-05-21 18:52:29 +0300

    Update mobile responsiveness

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,20 +1,32 @@
-import { Link } from "react-router-dom"
-import './HeaderStyles.css'
+import { Link } from "react-router-dom";
+import "./HeaderStyles.css";
+import { FaBars } from "react-icons/fa";
 
-export const Header =()=> {
-    return(
-        <>
-        <div className="navbar">
-            <div className="HeaderStyles.navbarLeft">
-                <h1>mwongess<span>.</span>me</h1>
-            </div>
-            <div className="navbarRight">
-                <p><Link to="">Portfolio</Link></p>
-                <p><Link to="">About</Link></p>
-                <p><Link to="">Contact</Link></p>
-                <button>Hire Me</button>
-            </div>
+export const Header = () => {
+  return (
+    <>
+      <div className="navbar">
+        <div className="navbarLeft">
+          <h1>
+            mwongess<span>.</span>me
+          </h1>
         </div>
-        </>
-    )
-}
+        <div className="navbarRight">
+          <p>
+            <Link to="">Portfolio</Link>
+          </p>
+          <p>
+            <Link to="">About</Link>
+          </p>
+          <p>
+            <Link to="">Contact</Link>
+          </p>
+          <button>Hire Me</button>
+        </div>
+        <div className="bars">
+          <FaBars />
+        </div>
+      </div>
+    </>
+  );
+};
--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -6,35 +6,18 @@ export const Landing = () => {
       <div className="landing">
         <h1>Amos Mwongela</h1>
         <h3>Software Engineer | Data Scientist</h3>
-        {/* <p>
-          I possess a comprehensive understanding of both frontend and backend
-          development.
-        </p>
-        <p>My proficiency in Angular,React, Vue & SvelteKit</p>
-        <p>
-          allows me to craft visually stunning and user-friendly interfaces that
-          captivate audiences.
-        </p>
-        <p>
-          On the backend, I am well-versed with languages such as NodeJs and
-          NestJs, enabling me to build robust and{" "}
-        </p>
-        <p>scalable server-side applications.</p> */}
 
         <p>
-        Are you looking for a skilled and versatile full stack developer to bring your digital projects to life?.Look no further than me.
-        </p>
-        <p> With a passion for coding and a talent for creating innovative solutions, Im your go-to professional when it comes to web and software development.</p>
-        {/* <p>
-          My proficiency in Angular,React, Vue & SvelteKit allows me to craft
-          visually stunning and user-friendly interfaces that captivate
-          audiences.
+          Are you looking for a skilled and versatile full stack developer to
+          bring your digital projects to life?.Look no further than me.
         </p>
         <p>
-          On the backend, I am well-versed with languages such as NodeJs and
-          NestJs, enabling me to build robust and scalable server-side
-          applications.
-        </p> */}
+          {" "}
+          With a passion for coding and a talent for creating innovative
+          solutions, Im your go-to professional when it comes to web and
+          software development.
+        </p>
+
         <div className="buttons">
           <button className="linkedin">
             <a href="https://www.linkedin.com/in/mwongess" target="_blank">
