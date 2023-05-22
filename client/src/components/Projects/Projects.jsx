a8f18f8ada1151faade0ebe25c42a45d29d927d2
mwongess <amosmwongelah@gmail.com>
2023-05-22 21:31:18 +0300

    Learn more

--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -1,15 +1,16 @@
-export const Projects = ({projects})=>{
-    return (
-        <>
-        <div>
-            {
-                projects.map((project,index)=>(
-                    <div key={index}>
-
-                    </div>
-                ))
-            }
-        </div>
-        </>
-    )
-}
+export const Projects = ({ projects }) => {
+  return (
+    <>
+      <div>
+        {projects.map((project, index) => (
+          <div key={index} className="project">
+            <img src={project.imageUrl} alt="" />
+            <h3></h3>
+            <p></p>
+            <button className="learn-more">Learn More</button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
