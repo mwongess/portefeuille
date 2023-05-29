fd1dbd775de1b44ad762a1b56760460400027cd9
mwongess <amosmwongelah@gmail.com>
2023-05-29 21:39:53 +0300

    Try scrolling effect

--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -4,7 +4,7 @@ import './ProjectsStyles.css'
 export const Projects = () => {
   return (
     <>
-      <div className="projects">
+      <div className="projects" id="projects">
         {projects.map((project, index) => (
           <div key={index} className="project">
             <img src={project.imageUrl} alt="" />
