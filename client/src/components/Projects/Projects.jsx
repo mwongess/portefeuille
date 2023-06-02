a18313a0a69b018aa3b28cc62078a0ea56b0820f
mwongess <amosmwongelah@gmail.com>
2023-06-02 19:56:29 +0300

    Update project images

--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -7,10 +7,15 @@ export const Projects = () => {
       <div className="projects" id="projects">
         {projects.map((project, index) => (
           <div key={index} className="project">
-            <img src={project.imageUrl} alt="" />
-            <h3>{project.name}</h3>
-            <p>{project.description}</p>
-            <button className="learn-more">Learn More</button>
+            <div className="project-image">
+              <img src={project.imageUrl} alt="" />
+            </div>
+            <div className="project-text">
+              <h3>{project.name}</h3>
+              <p>{project.description}</p>
+              <button className="learn-more">Learn More</button>
+            </div>
+
           </div>
         ))}
       </div>
