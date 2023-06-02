3f66c4ebb27788d810b6657f04ba606a670ca530
mwongess <amosmwongelah@gmail.com>
2023-06-02 20:35:53 +0300

    Update task x

--- client/src/data/data.js
+++ client/src/data/data.js
@@ -1,11 +1,10 @@
 export const projects = [
-
   {
-    name: "WaterTracker",
-    description: "Streamline water bill management effortlessly with our intuitive web app. Simplify tracking, payments, and analysis seamlessly ",
-    imageUrl: "/images/projectImages/watertracker.png",
-    website: "https://wartartracker.netlify.app/"
+    name: "Task-X",
+    description: "",
+    imageUrl: "/images/projectImages/taskx.webp",
   },
+ 
   {
     name: "UrlShortener",
     description: "The ultimate web app that transforms lengthy URLs into concise, shareable links with just a single click. ",
@@ -21,11 +20,7 @@ export const projects = [
     description: "",
     imageUrl: "/images/projectImages/job.png",
   },
-  {
-    name: "Task-X",
-    description: "",
-    imageUrl: "",
-  },
+ 
   {
     name: "SendIt",
     description:"Streamline your shipping process with our efficient web app for hassle-free delivery of your items.",
